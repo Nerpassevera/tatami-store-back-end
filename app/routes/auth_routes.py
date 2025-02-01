8ea7987930485ca88d02e9be10cc292853616f6f
Tatiana Trofimova <nerpassevera@gmail.com>
2025-01-31 20:59:18 -0800

    AWS Cognito configuration

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, redirect, url_for, session, jsonify
+from flask import Blueprint, redirect, url_for, session, jsonify, request
 from app.services.auth_services import oauth
 
 bp = Blueprint("auth", __name__)
@@ -13,6 +13,13 @@ def login():
 def callback():
     token = oauth.cognito.authorize_access_token()
     print("TOKEN RECEIVED:", token)
+    nonce = session.get("nonce", None)  # Stored in session (best practice)
+
+    if not nonce:
+        nonce = request.args.get("nonce")  # Try getting it from request
+
+    if not nonce:
+        return jsonify({"error": "Missing nonce"}), 400
     user = oauth.cognito.parse_id_token(token)
     session["user"] = user
     return jsonify({"message": "Logged in successfully", "user": user})
